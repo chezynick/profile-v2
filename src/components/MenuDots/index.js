8227703b5397d65f090f47385eaea586cd122f22
chezynick <chezynickannie@yahoo.co.uk>
2022-10-24 09:14:46 +0100

    responsive menu and larger buttons

--- src/components/MenuDots/index.js
+++ src/components/MenuDots/index.js
@@ -3,17 +3,22 @@ import cn from "classnames";
 import UseIsInViewport from "../../hooks/UseIsInViewport";
 
 const MenuDots = ({ refPoints, scrollToComponent }) => {
-  const dotStyle = "m-2 h-3 w-3 rounded-full flex-shrink-0 bg-gray-700";
+  const dotStyle =
+    " md:h-3 md:w-3 h-2 w-2 rounded-full flex-shrink-0 bg-gray-700";
   return (
-    <div className="fixed border  opacity-95 z-10  border-gray-700 rounded-lg p-2 ml-2 flex flex-col justify-center items-center left-0 top-1/3">
+    <div className="fixed border  opacity-95 z-10 py-2  border-gray-700 rounded-lg  md:ml-2  ml-1 flex flex-col justify-center items-center bottom-1 left-0 md:top-1/3 md:bottom-auto">
       {refPoints.map((page) => (
         <button
           key={page.name}
-          className={cn(dotStyle, {
-            " bg-primaryColor ": UseIsInViewport(page.midPoint),
-          })}
+          className="p-2 "
           onClick={() => scrollToComponent(page.mainRef)}
-        />
+        >
+          <div
+            className={cn(dotStyle, {
+              " bg-primaryColor ": UseIsInViewport(page.midPoint),
+            })}
+          />
+        </button>
       ))}
     </div>
   );
--- src/pages/Welcome/index.js
+++ src/pages/Welcome/index.js
@@ -5,7 +5,7 @@ const Welcome = ({ scrollToComponent, forwardRef, workRef, midRef }) => {
   return (
     <div
       ref={forwardRef}
-      className="bg-gray-800 z-50 opacity-100  text-white h-screen w-full flex flex-col justify-center text-4xl items-center"
+      className="bg-gray-800 p-4  z-50 opacity-100  text-white h-screen w-full flex flex-col justify-center text-4xl items-center"
     >
       <p ref={midRef}>
         Hello, My name is{" "}
