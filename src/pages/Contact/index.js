9f9f2e21150abb67f07eaa8c21e54aa7aec7a1a2
chezynick <chezynickannie@yahoo.co.uk>
2022-11-06 15:16:40 +0000

    added contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -16,20 +16,42 @@ const Contact = ({ forwardRef, midRef, scrollToComponent, topRef }) => {
       className="relative z-0 flex flex-col items-center justify-center w-full h-screen text-gray-100 bg-gray-800"
     >
       <PageHeader text="Contact" light />
-      <Button onClick={() => scrollToComponent(topRef)} text="Back to top" />
-      <div className="absolute flex justify-center w-full h-auto space-x-4 bottom-4">
-        <FontAwesomeIcon size="lg" icon={faGithub} className=" fill-white" />
-        <FontAwesomeIcon
-          size="lg"
-          icon={faLinkedinIn}
-          className=" fill-white"
-        />
-        <FontAwesomeIcon
-          size="lg"
-          icon={faInstagramSquare}
-          className=" fill-white"
-        />
-        <FontAwesomeIcon size="lg" icon={faEnvelope} className=" fill-white" />
+      <div className="flex justify-center w-full h-auto space-x-6 ">
+        <a target="_blank" href="https://github.com/chezynick" rel="noreferrer">
+          <FontAwesomeIcon size="3x" icon={faGithub} className=" fill-white" />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.linkedin.com/in/nick-chesterton-3b7818223/"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            size="3x"
+            icon={faLinkedinIn}
+            className=" fill-white"
+          />
+        </a>
+        <a
+          target="_blank"
+          href="https://www.instagram.com/chezynickannie/?hl=en"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon
+            size="3x"
+            icon={faInstagramSquare}
+            className=" fill-white"
+          />
+        </a>
+        <a href="malito:nick.chesterton@yahoo.com" rel="noreferrer">
+          <FontAwesomeIcon
+            size="3x"
+            icon={faEnvelope}
+            className=" fill-white"
+          />
+        </a>
+      </div>
+      <div className="absolute bottom-0 flex justify-center w-full">
+        <Button onClick={() => scrollToComponent(topRef)} text="Back to top" />
       </div>
       <div ref={midRef} className="absolute left-0 top-1/2" />
     </div>
