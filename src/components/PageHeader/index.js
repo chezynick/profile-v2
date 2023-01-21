9e4231aa7d59d671370bf056c6a697e988b1a5b4
chezynick <chezynickannie@yahoo.co.uk>
2023-01-21 17:00:45 +0000

    header size on mobile

--- src/components/PageHeader/index.js
+++ src/components/PageHeader/index.js
@@ -6,7 +6,7 @@ const PageHeader = ({ text, light = false }) => {
     <div className="absolute top-0 left-0 flex justify-center w-full pt-4 ">
       <div className="flex justify-center w-full md:w-3/4 sm:justify-start ">
         <h1
-          className={cn("text-3xl sm:pl-12 md:pl-0  text-gray-700", {
+          className={cn("sm:text-3xl text-xl sm:pl-12 md:pl-0  text-gray-700", {
             "text-gray-100": light,
           })}
         >
